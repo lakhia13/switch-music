bce9237d0592157fc60834dda715e84fa11f60df
Pranshav Lakhia <120188054+lakhia13@users.noreply.github.com>
2024-12-29 09:08:21 -0500

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,4 +1,3 @@
 # Spotify API credentials (replace with your credentials)
-# https://open.spotify.com/playlist/35twD7CGUjyz3vTG948WAD?si=65DN1XlvTnKoEeZtw7PBug&pi=j-iKCacHR_C8f
-CLIENT_ID_SP = '6d4bab7bddf6411f94810f9628e22550'
-CLIENT_SECRET_SP = '037280f853294013a1cf89cfda04295c'
+CLIENT_ID_SP = 'foo'
+CLIENT_SECRET_SP = 'bar'
